9c3319d61b8a53572132294add3f0b02b6fd8c97
razmy <razmyslovichv@gmail.com>
2021-10-25 21:20:51 +0200

    Task complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println(2021-6);
         //System.out.println("Родился в" 2015);
-        //System.out.println("2015");
+        System.out.println("2015");
         //System.out.println("В 2021-6");
     }
 }
